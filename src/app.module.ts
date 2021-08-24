39eb969c736ca01e5c296d7f0cef6d23e2dde5ac
IhorS <caxnoigor@gmail.com>
2021-08-24 09:38:16 +0300

    port

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CalcModule } from './Cacl/calc.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { RenderModule } from 'nest-next';
+import Next from 'next';
 import * as path from 'path';
 
 @Module({
   imports: [
     CalcModule,
+    RenderModule.forRootAsync(Next({ dev: true }), { viewsDir: null }),
     ServeStaticModule.forRoot({
       rootPath: path.join(__dirname, './', 'static'),
     }),
